d3aa5f265f5054ac971b45c6d13d241735790269
Park HyeongJin <jt3205@users.noreply.github.com>
2017-10-10 23:45:09 +0900

    2017-10-10

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var request = require('request');
 var bodyParser = require('body-parser');
+var async = require('async');
+var sleep = require('sleep');
 
 var app = express();
 var port = 3242;
@@ -14,28 +16,34 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
 app.use(bodyParser.json());
 
-app.use(function (req, res) {
-  res.setHeader('Content-Type', 'text/plain');
-  res.write('you posted:\n');
-  res.end(JSON.stringify(req.body, null, 2));
-});
 
 app.get('/', function (req, res) {
     res.render('index');
 });
 app.get('/clothes', function(req, res) {
-  res.json(303, getClothes('2017-10-08', '서울', 'male'));
-  res.render('clothes');
+  var clothes = null;
+  console.log(0);
+  async.waterfall([
+    function(callback){
+      clothes = getClothes('2017-10-10', '서울', 'male');
+      callback(clothes);
+    },
+    function(){
+      console.log(clothes);
+      res.send(clothes);
+    }
+  ]);
 });
 
-function getClothes(date, loaction, gender){
+//  res.render('clothes',getClothes('2017-10-10', '서울', 'male'));
+function getClothes(date, location, gender){
   var data = {
     date: date,
-    loaction: location,
+    location: location,
     gender: gender
   };
   request('http://www.fashionup.co.kr/api/clothes', data, function (error, response, body){
-      return body;
+    return body;
   });
 }
 
